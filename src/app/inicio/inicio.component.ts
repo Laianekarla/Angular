83837bbd0ab8090aedede56eac29fcbe7fcda985
Laiane <91036815+Laianekarla@users.noreply.github.com>
2021-12-07 14:09:23 -0300

    Consumo de API - Login e Cadastro

---
+++ src/app/inicio/inicio.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit {
+  foto: string = environment.foto
+  nome: string = environment.nome
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    if(environment.token == ''){
+      this.router.navigate(['/login'])
+    }
+  }
+
+}
